c7e260d5424678fd1e2d7fb6e2ce1c5409b5d131
Miles Ramage <miles.ramage@yahoo.com>
2024-04-18 13:33:52 -0500

    Updated settings for the rose-pine colorscheme.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,72 @@ return {
     "rose-pine/neovim",
     name = "rose-pine",
     lazy = true,
+    opts = {                                    -- DEFAULT SETTINGS:
+      variant = "auto",                         -- "auto"
+      dark_variant = "main",                    -- "main"
+      dim_inactive_windows = true,              -- false
+      extend_background_behind_borders = true,  -- true
+
+      enable = {
+        terminal = true,                        -- true
+        legacy_highlights = true,               -- true
+        migrations = true,                      -- true
+      },
+
+      styles = {
+        bold = true,                            -- true
+        italic = true,                          -- true
+        transparency = true,                    -- false
+      },
+
+      -- groups = {
+      --   border = "muted",
+      --   link = "iris",
+      --   panel = "surface",
+      --
+      --   error = "love",
+      --   hint = "iris",
+      --   info = "foam",
+      --   note = "pine",
+      --   todo = "rose",
+      --   warn = "gold",
+      --
+      --   git_add = "foam",
+      --   git_change = "rose",
+      --   git_delete = "love",
+      --   git_dirty = "rose",
+      --   git_ignore = "muted",
+      --   git_merge = "iris",
+      --   git_rename = "pine",
+      --   git_stage = "iris",
+      --   git_text = "rose",
+      --   git_untracked = "subtle",
+      --
+      --   h1 = "iris",
+      --   h2 = "foam",
+      --   h3 = "rose",
+      --   h4 = "gold",
+      --   h5 = "pine",
+      --   h6 = "foam",
+      -- },
+
+      highlight_groups = {
+        --Comment = { fg = "foam" },
+        --VertSplit = { fg = "muted", bg = "muted" },
+      },
+
+      before_highlight = function(group, highlight, pallette)
+        -- -- Disable all underculs
+        -- if highlight.undercurl then
+        --   highlight.undercurl = false
+        -- end
+        --
+        -- -- Change pallette colour
+        -- if highlight.fg == pallette.pine then
+        --   highlight.fg = pallette.foam
+        -- end
+      end,
+    },
   },
   {
     "folke/tokyonight.nvim",
